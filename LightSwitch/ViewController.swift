8afe5bc0a8c2ac3179d180e5294b0b3acc9404d3
Diego Sierra Puente <diegosiep@icloud.com>
2022-06-21 19:58:49 -0500

    LightSwitchy
    This is an application built on Swift that let's you change your display's colour from white to black and viceversa, in case your iPhone's Flash LED light is not working properly :)

--- LightSwitch/ViewController.swift
+++ LightSwitch/ViewController.swift
@@ -4,16 +4,34 @@
 //
 //  Created by Diego Sierra on 21/06/22.
 //
+// Lamp icon by Icons8 <a target="_blank" href="https://icons8.com/icon/MlHQMXJL0QCa/lamp">Lamp</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    
+    var lightSwitch = false
+    @IBOutlet var backgroundColour: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroundColour.backgroundColor = .black
+        
     }
-
-
+    
+    @IBAction func lightSwitcher(_ sender: UIButton) {
+        backgroundLight()
+        if lightSwitch {
+            backgroundColour.backgroundColor = .white
+        } else {
+            backgroundColour.backgroundColor = .black
+        }
+    }
+    
+    func backgroundLight() {
+        lightSwitch.toggle()
+        
+    }
+    
+    
 }
-
